268d7992bcfdbc85d5a65305f34ed92c9d2c60c9
beans_pc <ujt2h8@gmail.com>
2019-08-19 22:54:38 +0800

    [alexander][refactor][replace second if condition with switch]

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -42,11 +42,9 @@ public class Item {
             }
         }
 
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sellIn = sellIn - 1;
-        }
         switch (name) {
             case "Aged Brie":
+                sellIn = sellIn - 1;
                 if (sellIn < 0) {
                     if (quality < 50) {
                         quality = quality + 1;
@@ -54,6 +52,7 @@ public class Item {
                 }
                 break;
             case "Backstage passes to a TAFKAL80ETC concert":
+                sellIn = sellIn - 1;
                 if (sellIn < 0) {
                     quality = 0;
                 }
@@ -61,6 +60,7 @@ public class Item {
             case "Sulfuras, Hand of Ragnaros":
                 break;
             default:
+                sellIn = sellIn - 1;
                 if (sellIn < 0) {
                     if (quality > 0) {
                         quality = quality - 1;
@@ -68,22 +68,6 @@ public class Item {
                 }
                 break;
         }
-//            if (!name.equals("Aged Brie")) {
-//                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                    if (quality > 0) {
-//                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-//                            quality = quality - 1;
-//                        }
-//                    }
-//                } else {
-//                    quality = 0;
-//                }
-//            } else {
-//                if (quality < 50) {
-//                    quality = quality + 1;
-//                }
-//            }
-
     }
 
     @Override
