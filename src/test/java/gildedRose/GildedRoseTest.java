919b651b90c5db1eaa2787ff5f601083f9aa104a
beans_pc <ujt2h8@gmail.com>
2019-08-19 20:52:44 +0800

    [alexander][test][should_mentain_the_quality_when_item_name_not_equals_Aged_Brie_and_Backstage_passes_to_a_TAFKAL80ETC_concert_but_equals_Sulfuras_Hand_of_Ragnaros_and_quality_bigger_than_0]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class GildedRoseTest {
 
     @Test
-    public void should_sub_1_of_the_quality_when_item_name_not_equals_Aged_Brie_and_Backstage_passes_to_a_TAFKAL80ETC_concert_andSulfuras_Hand_of_Ragnaros_and_quality_bigger_than_0(){
+    public void should_sub_1_of_the_quality_when_item_name_not_equals_Aged_Brie_and_Backstage_passes_to_a_TAFKAL80ETC_concert_and_Sulfuras_Hand_of_Ragnaros_and_quality_bigger_than_0(){
         Item itemList[] = new Item[1];
         itemList[0] = new Item("test", 123,12);
         GildedRose gildedRose = new GildedRose(itemList);
@@ -18,5 +18,16 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
 
         assertEquals(11, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void should_mentain_the_quality_when_item_name_not_equals_Aged_Brie_and_Backstage_passes_to_a_TAFKAL80ETC_concert_but_equals_Sulfuras_Hand_of_Ragnaros_and_quality_bigger_than_0(){
+        Item itemList[] = new Item[1];
+        itemList[0] = new Item("Sulfuras, Hand of Ragnaros", 123,12);
+        GildedRose gildedRose = new GildedRose(itemList);
+
+        gildedRose.updateQuality();
+
+        assertEquals(12, gildedRose.items[0].quality);
     }
 }
