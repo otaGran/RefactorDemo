4830deae3bb207faba281d2cc698b46202b42a0b
beans_pc <ujt2h8@gmail.com>
2019-08-19 21:12:25 +0800

    [alexander][refactor][should_add_2_to_the_quality_when_item_name_equals__Backstage_passes_to_a_TAFKAL80ETC_concert_and_quality_equals_48_and_sellIn_smaller_than_11_bigger_than_5]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -97,6 +97,17 @@ public class GildedRoseTest {
         assertEquals(50, gildedRose.items[0].quality);
     }
 
+    @Test
+    public void should_add_2_to_the_quality_when_item_name_equals__Backstage_passes_to_a_TAFKAL80ETC_concert_and_quality_equals_48_and_sellIn_smaller_than_11_bigger_than_5() {
+        Item itemList[] = new Item[1];
+        itemList[0] = new Item("Aged Brie", 10, 49);
+        GildedRose gildedRose = new GildedRose(itemList);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, gildedRose.items[0].quality);
+    }
+
 
 
 
