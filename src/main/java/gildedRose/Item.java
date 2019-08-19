7f3ed60ae9656b5c8b41a7929c29b3202bc4e327
beans_pc <ujt2h8@gmail.com>
2019-08-19 22:59:54 +0800

    [alexander][refactor][merge nested if condition]

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -48,10 +48,8 @@ public class Item {
                     quality = quality - 1;
                 }
                 sellIn = sellIn - 1;
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        quality = quality - 1;
-                    }
+                if (sellIn < 0 && quality > 0) {
+                    quality = quality - 1;
                 }
                 break;
         }
