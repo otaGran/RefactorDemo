52e88a0d7e36c6ff114de5df0027720d7456a655
beans_pc <ujt2h8@gmail.com>
2019-08-19 22:59:13 +0800

    [alexander][refactor][merge nested if condition]

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -28,10 +28,8 @@ public class Item {
             case "Backstage passes to a TAFKAL80ETC concert":
                 if (quality < 50) {
                     quality = quality + 1;
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
+                    if (sellIn < 11 && quality < 50) {
+                        quality = quality + 1;
                     }
 
                     if (sellIn < 6) {
