a5a7788d563755d543405689683c6e076ad0c6e8
beans_pc <ujt2h8@gmail.com>
2019-08-19 23:13:19 +0800

    [alexander][create][OtherItemStrategy]

---
+++ src/main/java/gildedRose/OtherItemStrategy.java
@@ -0,0 +1,14 @@
+package gildedRose;
+
+public class OtherItemStrategy implements ItemStrategy {
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+}
